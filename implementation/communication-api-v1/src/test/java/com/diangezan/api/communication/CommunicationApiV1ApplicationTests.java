0c20a472b45d73f48d67db75deaeaec7b429d2f8
Lichader <1524470+lichader@users.noreply.github.com>
2020-07-30 16:28:25 +1000

    Add basic project files

---
+++ implementation/communication-api-v1/src/test/java/com/diangezan/api/communication/CommunicationApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diangezan.api.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
